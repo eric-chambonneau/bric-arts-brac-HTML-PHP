d4ddcdcf852c6eb1a4a913f19b349ded570b4cc7
eric-chambonneau <101542272+eric-chambonneau@users.noreply.github.com>
2022-10-20 22:15:22 +0200

    Add files via upload

---
+++ estimation.php
@@ -0,0 +1,14 @@
+<?php
+
+// on affiche que l'estimation du prix du rachat en cours
+echo "Estimation en cours...";
+
+// récupérer une donnée du formulaire
+//  echo $_POST['annee'];
+
+// variable pour récupérer l'année
+$nom = $_POST['nom'];
+$type = $_POST['type']
+$annee = $_POST['annee'];
+
+echo "l oeuvre s'appelle :" . $nom;
