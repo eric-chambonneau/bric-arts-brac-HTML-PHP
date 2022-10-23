399c9a8636d451939bf9db16ff14b54cb9c11c32
eric-chambonneau <101542272+eric-chambonneau@users.noreply.github.com>
2022-10-23 22:00:44 +0200

    Add files via upload

--- estimation.php
+++ estimation.php
@@ -1,14 +1,27 @@
 <?php
 
 // on affiche que l'estimation du prix du rachat en cours
-echo "Estimation en cours...";
+// echo " <h1>Estimation en cours...</h1> ";
 
 // récupérer une donnée du formulaire
 //  echo $_POST['annee'];
 
 // variable pour récupérer l'année
-$nom = $_POST['nom'];
-$type = $_POST['type']
-$annee = $_POST['annee'];
+// $nom = $_POST['nom'];
+// $type = $_POST['type'];
 
-echo "l oeuvre s'appelle :" . $nom;
+// extraire toutes les données du formulaire et de les mettre dans une variable
+extract($_POST);
+
+
+// condition
+if (!empty($nom) && !empty($type) && !empty($annee)) {
+    
+    echo  "<h1>Estimation en cours...</h1> " . "<br>";
+}
+else{
+    echo "Merci de remplir tous les champs <br>";
+}
+
+echo "l oeuvre s'appelle :" .$nom . "<br>";
+echo "Le type d'oeuvre est : " .$type . "<br>";
