989a7a744fc3cbc2a8cb3a2920e6c7d80600bf7a
eric-chambonneau <101542272+eric-chambonneau@users.noreply.github.com>
2022-10-24 21:59:04 +0200

    Add files via upload

--- estimation.php
+++ estimation.php
@@ -18,10 +18,41 @@ extract($_POST);
 if (!empty($nom) && !empty($type) && !empty($annee)) {
     
     echo  "<h1>Estimation en cours...</h1> " . "<br>";
+
+    //je récupère l'année de réaliqtion de l'oeuvre
+    echo "Année : " . $annee;
+
+    // vérification de l'année
+    if ($annee <= 1200) {
+        echo "100000";
+    }
+
+    else if ($annee > 1600 && $annee <= 1800 )
+    {
+        echo '76000'
+    }
+    
+    else if($annee > 1200 && $annee <= 1600) {
+        echo '32000';
+    }
+
+    else if($annee > 1800 && $annee <= 1999) {
+        echo '15000';
+    }
+    else {
+        echo "non accepté";
+    }
+
 }
+
+// vérification types d'oeuvre
+
+echo $type . "<br>";
+
+
 else{
     echo "Merci de remplir tous les champs <br>";
 }
 
-echo "l oeuvre s'appelle :" .$nom . "<br>";
-echo "Le type d'oeuvre est : " .$type . "<br>";
+// echo "l oeuvre s'appelle :" .$nom . "<br>";
+// echo "Le type d'oeuvre est : " .$type . "<br>";
